5c229e85153f7c26bc4c7fcbdb451f1feb83dc1b
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-02-07 10:11:04 +0100

    added headezr component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import { Header, Scoreboard, Cards, Footer, GameOver } from './components/';
 
 function App() {
   const [cats, setCats] = useState(catArr);
+  const [selected, setSelected] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highestScore, setHighestScore] = useState(0);
 
+  const resetGame = () => {
+    setScore(0);
+    setSelected([]);
+    setCats(catArr);
+  };
+
+  const onSelection = (obj) => {
+    if (selected.some((object) => object.name === obj.name)) {
+      resetGame();
+      return;
+    }
+
+    setScore(score + 1);
+    setSelected([...selected, obj]);
+    setCats([...cats.filter((cat) => cat.name !== obj.name)]);
+
+    if (score === highestScore) setHighestScore(highestScore + 1);
+  };
 
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
